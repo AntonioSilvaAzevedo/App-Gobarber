11387f97661c714f750a1667b9023fd8d1b2bf18
Antonio Carlos <antonie.dev@gmail.com>
2020-08-17 15:13:03 -0300

    firsm comit of monday

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
 const Dashboard: React.FC = () => {
-  const { singOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={singOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateProfile}>
+          <UserAvatar
+            source={{
+              uri:
+                'https://avatars3.githubusercontent.com/u/60106864?s=460&u=92a19a4a2f7f4214a0a052c6966ac558e8ee4b88&v=4',
+            }}
+          />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
