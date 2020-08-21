cdec14027fe83f1957f157d9feaad229cf656a76
Antonio Carlos <antonie.dev@gmail.com>
2020-08-21 19:17:26 -0300

    commit of day could

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  Okbutton,
+  OkButtonText,
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Pode escrever qualquer coisa se quiser</Description>
+
+      <Okbutton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </Okbutton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
